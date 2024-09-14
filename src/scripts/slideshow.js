25ae9d893ab99e7eeee698210378e1dd1c5f82a1
Jacquet12 <jacquetleme2021@gmail.com>
2024-09-14 03:10:57 -0300

    :sparkles: feat: slide implementation

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -1,24 +1,27 @@
 let slideIndex = 0;
 showSlides();
 
+// Função que mostra os slides
 function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
+    
+    // Oculta todos os slides
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none";  
     }
+    
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); 
+    
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    
+    // Exibe o slide atual
+    slides[slideIndex - 1].style.display = "block";  
+    
+    // Muda de slide a cada 5 segundos
+    setTimeout(showSlides, 5000);
 }
 
 function plusSlides(n) {
-    slideIndex += n;
-    let slides = document.getElementsByClassName("mySlides");
-    if (slideIndex > slides.length) {slideIndex = 1}
-    if (slideIndex < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+    slideIndex += n - 1;
+    showSlides();
 }
